cb6bb30c89bd6e4752118c12b0517cfc33f62890
Nick Rudzicz <nrudzicz@gmail.com>
2019-04-26 15:22:19 -0400

    .gitignore & OpenPlayerSettings

---
+++ Editor/OpenPlayerSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+/**
+ * Keyboard shortcut to open the Player Settings window with Ctrl-Shift-L
+ */
+public class OpenPlayerSettings {
+
+    [MenuItem("Nick/Windows/Open Player Settings %#l", false, 0)]
+    static void Foo() {
+        EditorApplication.ExecuteMenuItem("Edit/Project Settings/Player");
+        return;
+    }
+}
